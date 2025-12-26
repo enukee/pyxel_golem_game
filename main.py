a15b2c50d2998ec2d7801395a9fd27f1e2cfba37
vika <rodina2017g@yandex.ru>
2025-12-26 14:49:55 +0300

    Configuring the main file

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import pyxel
 
+from events import Events
 from MatrixMap import *
+from units import Player
+from events import PyxelController
+from draw import Scene
 
 
 class Game:
     def __init__(self):
-        self.scene = Scene(const.WINDOW_WIDTH, const.WINDOW_HEIGHT, "Game")
         self.tileMap = MatrixMap()
+        self.player = Player(30, 200)
+
+        self.scene = Scene(const.WINDOW_WIDTH, const.WINDOW_HEIGHT, "Game")
+        self.events = Events(PyxelController())
+
+        def movement(dirX, dirY):
+            self.player.update(dirX, dirY, self.tileMap)
+
+        self.events.addPlayerMovementHandler(movement)
 
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        pass
+        self.events.update()
 
     def draw(self):
         # Очистка экрана
         pyxel.cls(1)
-        self.tileMap.draw(self.scene, 300, 300)
+
+        self.tileMap.draw(self.scene, self.player.x, self.player.y)
+        self.player.draw(self.scene)
 
 
 if __name__ == "__main__":
