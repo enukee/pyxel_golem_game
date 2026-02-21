787d813d554207dc3acda2f7c66a2e72d04646be
vika <rodina2017g@yandex.ru>
2026-02-21 18:28:44 +0300

    Transferring implementations of keystroke verification methods from PyxelController to the base Controller class

--- events/Controller.py
+++ events/Controller.py
@@ -2,30 +2,67 @@ from abc import ABC, abstractmethod
 
 
 class Controller(ABC):
+    def __init__(self, buttonLeft, buttonRight, buttonDown, buttonUp, buttonShoot, buttonRaise):
+        self.buttonLeft = buttonLeft
+        self.buttonRight = buttonRight
+        self.buttonDown = buttonDown
+        self.buttonUp = buttonUp
+        self.buttonShoot = buttonShoot
+        self.buttonRaise = buttonRaise
+
     @abstractmethod
+    def isBtnPress(self, btn):
+        pass
+
     def isLeftButtonPressed(self) -> bool:
-        pass
+        if self.isBtnPress(self.buttonLeft):
+            return True
 
-    @abstractmethod
+        return False
+
     def isRightButtonPressed(self) -> bool:
-        pass
+        if self.isBtnPress(self.buttonRight):
+            return True
 
-    @abstractmethod
+        return False
+
     def isDownButtonPressed(self) -> bool:
-        pass
+        if self.isBtnPress(self.buttonDown):
+            return True
 
-    @abstractmethod
+        return False
+
     def isUpButtonPressed(self) -> bool:
-        pass
+        if self.isBtnPress(self.buttonUp):
+            return True
 
-    @abstractmethod
+        return False
+
     def isShootButtonPressed(self) -> bool:
-        pass
+        if self.isBtnPress(self.buttonShoot):
+            return True
 
-    @abstractmethod
+        return False
+
     def isRaiseButtonPressed(self) -> bool:
-        pass
+        if self.isBtnPress(self.buttonRaise):
+            return True
 
-    @abstractmethod
+        return False
+
     def getDirection(self) -> (int, int):
-        pass
+        dx, dy = 0, 0
+
+        if self.isLeftButtonPressed():
+            dx += -1
+
+        if self.isRightButtonPressed():
+            dx += 1
+
+        if self.isUpButtonPressed():
+            dy += -1
+
+        if self.isDownButtonPressed():
+            dy += 1
+
+        return dx, dy
--- events/PyxelController.py
+++ events/PyxelController.py
@@ -5,62 +5,8 @@ import pyxel
 
 class PyxelController(Controller):
     def __init__(self):
-        self.buttonLeft = pyxel.KEY_A
-        self.buttonRight = pyxel.KEY_D
-        self.buttonDown = pyxel.KEY_S
-        self.buttonUp = pyxel.KEY_W
-        self.buttonShoot = pyxel.KEY_KP_ENTER
-        self.buttonRaise = pyxel.KEY_E
+        super().__init__(pyxel.KEY_A, pyxel.KEY_D, pyxel.KEY_S, pyxel.KEY_W,
+                         pyxel.KEY_KP_ENTER, pyxel.KEY_E)
 
-    def isLeftButtonPressed(self) -> bool:
-        if pyxel.btn(self.buttonLeft):
-            return True
-
-        return False
-
-    def isRightButtonPressed(self) -> bool:
-        if pyxel.btn(self.buttonRight):
-            return True
-
-        return False
-
-    def isDownButtonPressed(self) -> bool:
-        if pyxel.btn(self.buttonDown):
-            return True
-
-        return False
-
-    def isUpButtonPressed(self) -> bool:
-        if pyxel.btn(self.buttonUp):
-            return True
-
-        return False
-
-    def isShootButtonPressed(self) -> bool:
-        if pyxel.btn(self.buttonShoot):
-            return True
-
-        return False
-
-    def isRaiseButtonPressed(self) -> bool:
-        if pyxel.btn(self.buttonRaise):
-            return True
-
-        return False
-
-    def getDirection(self) -> (int, int):
-        dx, dy = 0, 0
-
-        if self.isLeftButtonPressed():
-            dx += -1
-
-        if self.isRightButtonPressed():
-            dx += 1
-
-        if self.isUpButtonPressed():
-            dy += -1
-
-        if self.isDownButtonPressed():
-            dy += 1
-
-        return dx, dy
+    def isBtnPress(self, btn):
+        pyxel.btn(btn)
